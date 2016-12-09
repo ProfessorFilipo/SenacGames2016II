6a6bdd769cd92a11bb62816a01df34739a6a4ac9
ProfessorFilipo <filipo.mor@gmail.com>
2016-12-09 12:10:48 -0200

    Aula 09/dez/2016 - pontuacao e nome do usuario
    Inserindo pontuacao (com numeros alinhados a direita), quantidade de vidas e nome do usuario na barra inferior.

--- CataToca/main.c
+++ CataToca/main.c
@@ -31,7 +31,10 @@ void TelaAbertura(char *Nome)
 	printf("     U M   J O G O   D E   I N T E L I G E N C I A      \n");
 	printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
 	printf(" Digite seu nome: ");
-	scanf("%s", Nome);
+	//scanf("%s", Nome);
+	scanf("%[^\n]", Nome); // mascara para ler a linha inteira 
+	                       // digitada pelo usuario, ignorando
+	                       // os espacos em branco.
 }
 
 void TelaInicioFase(int NroFase)
@@ -61,10 +64,11 @@ void TelaInicioFase(int NroFase)
 	getch();
 }
 
-void MontaTabuleiro(char M[_Tab_H_][_Tab_W_], int Acertos /*, int Erros, int Vidas*/)
+void MontaTabuleiro(char M[_Tab_H_][_Tab_W_], int Pontos, int Vidas, char* Nome)
 {
 	int i, j;
 
+	// teste teste - enche tudo de pontos
 	for(i=0; i < _Tab_H_; i++)
 	{
 		for(j=0; j < _Tab_W_; j++)
@@ -110,20 +114,48 @@ void MontaTabuleiro(char M[_Tab_H_][_Tab_W_], int Acertos /*, int Erros, int Vid
     
     // Barra de Informacoes
     M[_Tab_H_-1][0] = 245; //M[_Tab_H_-1][1] = ':'; M[_Tab_H_-1][2] = ':';
-    M[_Tab_H_-1][1] = 'A'; 
-	M[_Tab_H_-1][2] = 'C'; 
-	M[_Tab_H_-1][3] = 'E'; 
-	M[_Tab_H_-1][4] = 'R'; 
-	M[_Tab_H_-1][5] = 'T'; 
-	M[_Tab_H_-1][6] = 'O'; 
-	M[_Tab_H_-1][7] = 'S';
-	M[_Tab_H_-1][8] = ':';
-	// imprime acertos
-	for(j=0; j<Acertos; j++)
+    M[_Tab_H_-1][1] = 'V'; 
+	M[_Tab_H_-1][2] = 'I'; 
+	M[_Tab_H_-1][3] = 'D'; 
+	M[_Tab_H_-1][4] = 'A'; 
+	M[_Tab_H_-1][5] = 'S'; 
+	M[_Tab_H_-1][6] = ':';
+	
+	// imprime Vidas restantes
+	for(j=0; j<5; j++) M[_Tab_H_-1][7+j] = 241; // preenche com cruzes
+	for(j=0; j<Vidas; j++) // agora, preenche com as vidas correspondentes.
 	{
-		M[_Tab_H_-1][9+j] = 184;
+		M[_Tab_H_-1][7+j] = 001;
 	}
-    
+	
+	// imprime pontos
+	M[_Tab_H_-1][20] = 'P';
+	M[_Tab_H_-1][21] = 'O';
+	M[_Tab_H_-1][22] = 'N';
+	M[_Tab_H_-1][23] = 'T';
+	M[_Tab_H_-1][24] = 'O';
+	M[_Tab_H_-1][25] = 'S';
+	M[_Tab_H_-1][26] = ':';
+	
+	char pontos[7]; // 7 = quantidade maxima de digitos + um digito para terminador.
+	sprintf( pontos,"%6d", Pontos); // o %6d garante o alinhamento do nro a direita.
+	// sprintf "imprime" em 'pontos' a string correspondente ao valor de 'Pontos'.
+	for(j=0; j<6; j++)
+	   M[_Tab_H_-1][27+j] = pontos[j];
+
+    // imprime nome do jogador
+    for(j=0; j<50; j++)
+    {
+       if(Nome[j] == NULL)
+	   {
+	   	   break;
+	   }   else M[_Tab_H_-1][35+j] = Nome[j];
+	}
+    /*for(j=0; j<50; j++)
+    {
+	   M[_Tab_H_-1][35+j] = Nome[j];
+	}*/
+       
 }
 
 void MostraTela( char M[_Tab_H_][_Tab_W_] )
@@ -154,9 +186,10 @@ int main(int argc, char *argv[]) {
     
     char Tabuleiro[_Tab_H_][_Tab_W_];
 
+    system("mode 100, 120");
     TelaAbertura   (Nome);
     TelaInicioFase (1);
-    MontaTabuleiro (Tabuleiro, 8);
+    MontaTabuleiro (Tabuleiro, 7777, 3, Nome);
     MostraTela     (Tabuleiro);
     
     //printf("\n %s\n ", Nome);
